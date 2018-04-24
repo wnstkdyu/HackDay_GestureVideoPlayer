c769d22ea2e5509307f22b675cbac37110ad8c64
alpaca <wnstkdyu@gmail.com>
2018-04-24 13:34:44 +0900

    playerView로 view 로직 옮김

--- HackDay_GestureVideoPlayer/Controller/PlayerViewController.swift
+++ HackDay_GestureVideoPlayer/Controller/PlayerViewController.swift
@@ -18,42 +18,20 @@ enum Direction {
 class PlayerViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var playerView: PlayerView!
     @IBOutlet weak var mediaSelectionTableView: UITableView!
-    @IBOutlet var outletCollection: [UIView]!
-    @IBOutlet weak var backButton: UIButton!
-    @IBOutlet weak var playButton: UIButton!
-    @IBOutlet weak var lockButton: UIButton!
-    @IBOutlet weak var timeSlider: UISlider!
-    @IBOutlet weak var currentTimeLabel: UILabel!
-    @IBOutlet weak var totalTimeLabel: UILabel!
-    @IBOutlet weak var subtitleButton: UIButton!
-    @IBOutlet weak var resolutionButton: UIButton!
-    
-    @IBOutlet weak var centerTimeLabel: UILabel!
     
     @IBOutlet weak var tapGestureRecognizer: UITapGestureRecognizer!
     @IBOutlet weak var doubleTapGestureRecognizer: UITapGestureRecognizer!
     
-    private lazy var activityIndicator: UIActivityIndicatorView = {
-        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .white)
-        activityIndicator.frame = CGRect(origin: playerView.center, size: CGSize(width: 40, height: 40))
-        
-        return activityIndicator
-    }()
-    
-    private lazy var expandingView: UIView = {
-        let expandingView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: playerView.frame.width / 2, height: playerView.frame.height)))
-        expandingView.backgroundColor = currentTimeLabel.textColor
-        expandingView.alpha = 0.5
-        
-        return expandingView
-    }()
-    
-    // PlayerManager
+    // PlayerManager 인스턴스 - VideoListView에서 초기화 시켜줌
     var playerManager: PlayerManager?
     
-    private var isVisible: Bool = true
+    private var isVisible: Bool = true {
+        didSet {
+            print("isVisible: \(isVisible)")
+        }
+    }
     private var isLocked: Bool = false
     private var workItemArray: [DispatchWorkItem] = []
     
@@ -76,6 +54,7 @@ class PlayerViewController: UIViewController {
         super.viewDidLoad()
         
         playerManager?.delegate = self
+        playerView.delegate = self
         
         tapGestureRecognizer.require(toFail: doubleTapGestureRecognizer)
         
@@ -86,8 +65,8 @@ class PlayerViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        hideUI()
-        changeToLandscape()
+        playerView.hideUI()
+        playerView.changeToLandscape()
     }
     
     // 화면 크기가 다 결정되고 나서 해야 비디오가 꽉 차게 나옴.
@@ -95,71 +74,16 @@ class PlayerViewController: UIViewController {
         super.viewDidAppear(animated)
         
         playerManager?.prepareToPlay()
+        
         // PlayerView에서 준비할 것들.
         playerManager?.playerLayer.frame = playerView.frame
         
         guard let asset = playerManager?.asset else { return }
-        setPlayerUI(asset: asset)
+        playerView.setPlayerUI(asset: asset)
         getSubtitleInfo(asset: asset)
 
         guard let playerLayer = playerManager?.playerLayer else { return }
-        playerView.layer.addSublayer(playerLayer)
-    }
-    
-    @IBAction func backButtonTapped(_ sender: UIButton) {
-        navigationController?.popViewController(animated: true)
-    }
-    
-    @IBAction func playButtonTapped(_ sender: UIButton) {
-        switch sender.isSelected {
-        case true:
-            playerManager?.pause()
-        case false:
-            playerManager?.play()
-        }
-    }
-    
-    @IBAction func replayButtonTapped(_ sender: UIButton) {
-        playerManager?.changeTenSeconds(to: .back)
-    }
-    
-    @IBAction func forwardButtonTapped(_ sender: UIButton) {
-        playerManager?.changeTenSeconds(to: .forward)
-    }
-    
-    @IBAction func lockButtonTapped(_ sender: UIButton) {
-        sender.isSelected = !sender.isSelected
-        
-        switch sender.isSelected {
-        case true:
-            setLockUI(isLocked: true)
-        case false:
-            setLockUI(isLocked: false)
-        }
-    }
-    
-    @IBAction func timeSliderValueChanged(_ sender: UISlider) {
-        let valueRatio = sender.value / sender.maximumValue
-        
-        guard let totoalSeconds = playerManager?.player.currentItem?.duration.seconds else { return }
-        let currentTimeSeconds = Double(valueRatio) * totoalSeconds
-        let timeToBeChanged = CMTime(seconds: currentTimeSeconds, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
-        
-        playerManager?.stopPlayingAndSeekSmoothlyToTime(newChaseTime: timeToBeChanged)
-    }
-    
-    @IBAction func subtitleButtonTapped(_ sender: UIButton) {
-        mediaSelectionDataSource.setSubtitleDataSource()
-        mediaSelectionTableView.reloadData()
-        
-        showMediaSelectionTableView()
-    }
-    
-    @IBAction func resolutionButtonTapped(_ sender: UIButton) {
-        mediaSelectionDataSource.setResolutionDataSource()
-        mediaSelectionTableView.reloadData()
-        
-        showMediaSelectionTableView()
+        playerView.layer.sublayers?.insert(playerLayer, at: 0)
     }
     
     @IBAction func handleTapGesture(_ sender: UITapGestureRecognizer) {
@@ -167,15 +91,13 @@ class PlayerViewController: UIViewController {
         hideMediaSelectionTableView()
         
         if isVisible {
-            fadeOutUI(isLocked: isLocked)
+            playerView.fadeOutUI(isLocked: isLocked)
         } else {
-            fadeInUI(isLocked: isLocked)
+            playerView.fadeInUI(isLocked: isLocked)
         }
     }
     
     @IBAction func handleDoubleTapGesture(_ sender: UITapGestureRecognizer) {
-        print(scrollView.zoomScale)
-        
         if scrollView.zoomScale == 1.0 {
             scrollView.setZoomScale(1.5, animated: true)
         } else {
@@ -186,11 +108,11 @@ class PlayerViewController: UIViewController {
     @IBAction func handlePanGesture(_ sender: UIPanGestureRecognizer) {
         switch sender.state {
         case .began:
-            centerTimeLabel.isHidden = false
+            playerView.centerTimeLabel.isHidden = false
             firstBrightness = UIScreen.main.brightness
             firstVolume = playerManager?.player.volume
             
-            view.addSubview(expandingView)
+            view.addSubview(playerView.expandingView)
         case .changed:
             let translation = sender.translation(in: playerView)
             let xRatio = translation.x / playerView.frame.width
@@ -206,8 +128,8 @@ class PlayerViewController: UIViewController {
                 if newSeconds > assetDuration {
                     newSeconds = assetDuration
                 }
-                newCMTime  = CMTime(seconds: newSeconds, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
-                centerTimeLabel.text = newCMTime?.toTimeForamt
+                newCMTime = CMTime(seconds: newSeconds, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+                playerView.centerTimeLabel.text = newCMTime?.toTimeForamt
             } else {
                 // 상하로 움직임 -> 화면을 반으로 갈라 왼쪽은 밝기, 오른쪽은 볼륨 조절
                 if sender.location(in: playerView).x <= playerView.frame.width / 2 {
@@ -215,15 +137,15 @@ class PlayerViewController: UIViewController {
                     guard let firstBrightness = firstBrightness else { return }
                     UIScreen.main.brightness = CGFloat(firstBrightness) - yRatio
                     
-                    expandingView.frame.origin = CGPoint(x: 0, y: playerView.frame.height * (1 - UIScreen.main.brightness))
+                    playerView.expandingView.frame.origin = CGPoint(x: 0, y: playerView.frame.height * (1 - UIScreen.main.brightness))
                 } else {
                     // 오른쪽: 볼륨
                     guard let firstVolume = firstVolume else { return }
                     playerManager?.player.volume = firstVolume - Float(yRatio)
                     
-                    expandingView.frame.origin = CGPoint(x: playerView.frame.width / 2, y: playerView.frame.height * CGFloat(1 - (firstVolume - Float(yRatio))))
-                    if expandingView.frame.origin.y < 0 {
-                        expandingView.frame.origin.y = 0
+                    playerView.expandingView.frame.origin = CGPoint(x: playerView.frame.width / 2, y: playerView.frame.height * CGFloat(1 - (firstVolume - Float(yRatio))))
+                    if playerView.expandingView.frame.origin.y < 0 {
+                        playerView.expandingView.frame.origin.y = 0
                     }
                 }
             }
@@ -245,27 +167,13 @@ class PlayerViewController: UIViewController {
             firstBrightness = nil
             firstVolume = nil
             
-            centerTimeLabel.text = nil
-            centerTimeLabel.isHidden = true
+            playerView.centerTimeLabel.text = nil
+            playerView.centerTimeLabel.isHidden = true
             
-            expandingView.frame.origin = CGPoint(x: 0, y: playerView.frame.maxY)
-            expandingView.removeFromSuperview()
+            playerView.expandingView.frame.origin = CGPoint(x: 0, y: playerView.frame.maxY)
+            playerView.expandingView.removeFromSuperview()
         default:
             break
-        }
-    }
-    
-    private func changeToLandscape() {
-        let orientationValue = UIDeviceOrientation.landscapeRight.rawValue
-        UIDevice.current.setValue(orientationValue, forKey: "orientation")
-    }
-    
-    private func setPlayerUI(asset: AVAsset) {
-        totalTimeLabel.text = asset.duration.toTimeForamt
-        if asset.duration.seconds / 3600 > 0 {
-            currentTimeLabel.text = "00:00:00"
-        } else {
-            currentTimeLabel.text = "00:00"
         }
     }
     
@@ -286,20 +194,8 @@ class PlayerViewController: UIViewController {
         mediaSelectionDataSource.subtitlesArray.append((.none, true))
     }
     
-    private func showUI() {
-        outletCollection.filter { $0 != backButton }
-            .forEach { $0.isHidden = false }
-        hideActivityIndicator()
-    }
-    
-    private func hideUI() {
-        outletCollection.filter { $0 != backButton }
-            .forEach { $0.isHidden = true }
-        showActivityIndicator()
-    }
-    
     private func showMediaSelectionTableView() {
-        outletCollection.forEach { $0.isHidden = true }
+        playerView.outletCollection.forEach { $0.isHidden = true }
         
         UIView.animate(withDuration: 0.3) { [weak self] in
             guard let tableViewFrame = self?.mediaSelectionTableView.frame,
@@ -309,124 +205,36 @@ class PlayerViewController: UIViewController {
     }
     
     private func hideMediaSelectionTableView() {
-        outletCollection.forEach { $0.isHidden = false }
+        playerView.outletCollection.forEach { $0.isHidden = false }
         
         UIView.animate(withDuration: 0.3) { [weak self] in
             guard let viewHeight = self?.view.frame.height else { return }
             self?.mediaSelectionTableView.frame.origin.y = viewHeight
         }
     }
-    
-    private func showActivityIndicator() {
-        guard !activityIndicator.isDescendant(of: self.playerView) else {
-            activityIndicator.startAnimating()
-            
-            return
-        }
-        
-        self.playerView.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
-    }
-    
-    private func hideActivityIndicator() {
-        activityIndicator.stopAnimating()
-    }
-    
-    private func fadeInUI(isLocked: Bool = false) {
-        outletCollection.filter { isLocked ? $0 == lockButton : true }
-            .forEach { outlet in
-                UIView.animate(withDuration: 1.0,
-                               delay: 0.0,
-                               options: [.curveEaseInOut, .allowUserInteraction, .beginFromCurrentState],
-                               animations: {
-                                outlet.alpha = 1.0
-                }, completion: { [weak self] _ in
-                    if isLocked {
-                        let workItem = DispatchWorkItem {
-                            UIView.animate(withDuration: 1.0,
-                                           animations: { [weak self] in
-                                            self?.lockButton.alpha = 0.0
-                            }, completion: { [weak self] _ in
-                                    self?.isVisible = false
-                            })
-                        }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: workItem)
-                    }
-                    guard outlet == self?.outletCollection.last else { return }
-                    self?.isVisible = true
-                    
-                    // isLocked일 경우 isLocked도 사라져야 함.
-                    let workItem = DispatchWorkItem {
-                        guard let strongSelf = self else { return }
-                        self?.fadeOutUI(isLocked: strongSelf.isLocked)
-                    }
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: workItem)
-                })
-        }
-    }
-    
-    @objc private func fadeOutUI(isLocked: Bool = false) {
-        outletCollection.filter { isLocked ? $0 != lockButton : true }
-            .forEach { outlet in
-                UIView.animate(withDuration: 1.0,
-                               delay: 0.0,
-                               options: [.curveEaseInOut, .allowUserInteraction, .beginFromCurrentState],
-                               animations: {
-                                outlet.alpha = 0.0
-                }, completion: { [weak self] _ in
-                    if isLocked {
-                        let workItem = DispatchWorkItem {
-                            UIView.animate(withDuration: 1.0,
-                                           animations: { [weak self] in
-                                            self?.lockButton.alpha = 0.0
-                            }, completion: { [weak self] _ in
-                                    self?.isVisible = false
-                            })
-                        }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: workItem)
-                    }
-                    guard outlet == self?.outletCollection.last else { return }
-                    
-                    self?.isVisible = false
-                })
-        }
-    }
-    
-    private func setLockUI(isLocked: Bool) {
-        self.isLocked = isLocked
-        print(self.isLocked)
-        
-        workItemArray.forEach { $0.cancel() }
-        switch isLocked {
-        case true:
-            fadeOutUI(isLocked: true)
-        case false:
-            fadeInUI(isLocked: false)
-        }
-    }
 }
 
 extension PlayerViewController: PlayerManagerDelegate {
     func isPlayedFirst() {
-        showUI()
+        playerView.showUI()
         playerManager?.play()
     }
     
     func setTimeObserverValue(time: CMTime) {
-        currentTimeLabel.text = time.seconds.toTimeFormat
+        playerView.currentTimeLabel.text = time.seconds.toTimeFormat
         
-        let timeSliderMaximumValue = timeSlider.maximumValue
+        let timeSliderMaximumValue = playerView.timeSlider.maximumValue
         guard let assetDuration = playerManager?.player.currentItem?.duration.seconds else { return }
         let value = timeSliderMaximumValue * Float(time.seconds / assetDuration)
-        timeSlider.setValue(value, animated: true)
+        playerView.timeSlider.setValue(value, animated: true)
     }
     
     func playerPlayed() {
-        playButton.isSelected = true
+        playerView.playButton.isSelected = true
         
         guard isVisible else { return }
         let workItem = DispatchWorkItem { [weak self] in
-            self?.fadeOutUI()
+            self?.playerView.fadeOutUI()
         }
         
         workItemArray.append(workItem)
@@ -436,27 +244,91 @@ extension PlayerViewController: PlayerManagerDelegate {
     }
     
     func playerPaused() {
-        playButton.isSelected = false
+        playerView.playButton.isSelected = false
         
         guard isVisible else { return }
         let workItem = DispatchWorkItem { [weak self] in
-            self?.fadeOutUI()
+            self?.playerView.fadeOutUI()
         }
         workItemArray.append(workItem)
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: workItem)
     }
 }
 
+extension PlayerViewController: PlayerViewDelegate {
+    func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func playButtonTapped(isSelected: Bool) {
+        switch isSelected {
+        case true:
+            playerManager?.pause()
+        case false:
+            playerManager?.play()
+        }
+    }
+    
+    func replayButtonTapped() {
+        playerManager?.changeTenSeconds(to: .back)
+    }
+    
+    func forwardButtonTapped() {
+        playerManager?.changeTenSeconds(to: .forward)
+    }
+    
+    func timeSliderValueChanged(value: Float) {
+        let valueRatio = value / playerView.timeSlider.maximumValue
+        
+        guard let totoalSeconds = playerManager?.player.currentItem?.duration.seconds else { return }
+        let currentTimeSeconds = Double(valueRatio) * totoalSeconds
+        let timeToBeChanged = CMTime(seconds: currentTimeSeconds, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        
+        playerManager?.stopPlayingAndSeekSmoothlyToTime(newChaseTime: timeToBeChanged)
+    }
+    
+    func subtitleButtonTapped() {
+        mediaSelectionDataSource.setSubtitleDataSource()
+        mediaSelectionTableView.reloadData()
+        
+        showMediaSelectionTableView()
+    }
+    
+    func resolutionButtonTapped() {
+        mediaSelectionDataSource.setResolutionDataSource()
+        mediaSelectionTableView.reloadData()
+        
+        showMediaSelectionTableView()
+    }
+    
+    func isVisibleChange(to isVisible: Bool) {
+        self.isVisible = isVisible
+    }
+    
+    func checkLockButton() {
+        playerView.fadeOutUI(isLocked: isLocked)
+    }
+    
+    func setLock(isLocked: Bool) {
+        self.isLocked = isLocked
+        workItemArray.forEach { $0.cancel() }
+    }
+}
+
 extension PlayerViewController: UIGestureRecognizerDelegate {
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        
+
         guard let touchView = touch.view,
             touchView.isDescendant(of: view) else { return false }
         return true
     }
-    
+
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return false
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
 
--- HackDay_GestureVideoPlayer/View/PlayerView.swift
+++ HackDay_GestureVideoPlayer/View/PlayerView.swift
@@ -7,7 +7,195 @@
 //
 
 import UIKit
+import AVFoundation
+
+protocol PlayerViewDelegate: class {
+    func backButtonTapped()
+    func playButtonTapped(isSelected: Bool)
+    func replayButtonTapped()
+    func forwardButtonTapped()
+    func timeSliderValueChanged(value: Float)
+    func subtitleButtonTapped()
+    func resolutionButtonTapped()
+    
+    func isVisibleChange(to isVisible: Bool)
+    func checkLockButton()
+    func setLock(isLocked: Bool)
+}
 
 class PlayerView: UIView {
+    @IBOutlet var outletCollection: [UIView]!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var lockButton: UIButton!
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    @IBOutlet weak var subtitleButton: UIButton!
+    @IBOutlet weak var resolutionButton: UIButton!
     
+    @IBOutlet weak var centerTimeLabel: UILabel!
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .white)
+        activityIndicator.frame = CGRect(origin: self.center, size: CGSize(width: 40, height: 40))
+        
+        return activityIndicator
+    }()
+    
+    lazy var expandingView: UIView = {
+        let expandingView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: self.frame.width / 2, height: self.frame.height)))
+        expandingView.backgroundColor = currentTimeLabel.textColor
+        expandingView.alpha = 0.5
+        
+        return expandingView
+    }()
+    
+    weak var delegate: PlayerViewDelegate?
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        delegate?.backButtonTapped()
+    }
+    
+    @IBAction func playButtonTapped(_ sender: UIButton) {
+        delegate?.playButtonTapped(isSelected: sender.isSelected)
+    }
+    
+    @IBAction func replayButtonTapped(_ sender: UIButton) {
+        delegate?.replayButtonTapped()
+    }
+    
+    @IBAction func forwardButtonTapped(_ sender: UIButton) {
+        delegate?.forwardButtonTapped()
+    }
+    
+    @IBAction func lockButtonTapped(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        setLockUI(isLocked: sender.isSelected)
+    }
+    
+    @IBAction func timeSliderValueChanged(_ sender: UISlider) {
+        delegate?.timeSliderValueChanged(value: sender.value)
+    }
+    
+    @IBAction func subtitleButtonTapped(_ sender: UIButton) {
+        delegate?.subtitleButtonTapped()
+    }
+    
+    @IBAction func resolutionButtonTapped(_ sender: UIButton) {
+        delegate?.resolutionButtonTapped()
+    }
+    
+    func changeToLandscape() {
+        let orientationValue = UIDeviceOrientation.landscapeRight.rawValue
+        UIDevice.current.setValue(orientationValue, forKey: "orientation")
+    }
+    
+    func setPlayerUI(asset: AVAsset) {
+        totalTimeLabel.text = asset.duration.toTimeForamt
+        if asset.duration.seconds / 3600 > 0 {
+            currentTimeLabel.text = "00:00:00"
+        } else {
+            currentTimeLabel.text = "00:00"
+        }
+    }
+    
+    func showUI() {
+        outletCollection.filter { $0 != backButton }
+            .forEach { $0.isHidden = false }
+        hideActivityIndicator()
+    }
+    
+    func hideUI() {
+        outletCollection.filter { $0 != backButton }
+            .forEach { $0.isHidden = true }
+        showActivityIndicator()
+        
+//        delegate?.isVisibleChange(to: false)
+    }
+    
+    func showActivityIndicator() {
+        guard !activityIndicator.isDescendant(of: self) else {
+            activityIndicator.startAnimating()
+            
+            return
+        }
+        
+        self.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+    }
+    
+    func hideActivityIndicator() {
+        activityIndicator.stopAnimating()
+    }
+    
+    func fadeInUI(isLocked: Bool = false) {
+        outletCollection.filter { isLocked ? $0 == lockButton : true }
+            .forEach { outlet in
+                UIView.animate(withDuration: 1.0,
+                               delay: 0.0,
+                               options: [.curveEaseInOut, .allowUserInteraction, .beginFromCurrentState],
+                               animations: {
+                                outlet.alpha = 1.0
+                }, completion: { [weak self] _ in
+                    if isLocked {
+                        let workItem = DispatchWorkItem {
+                            UIView.animate(withDuration: 1.0,
+                                           animations: { [weak self] in
+                                            self?.lockButton.alpha = 0.0
+                                }, completion: { [weak self] _ in
+                                    self?.delegate?.isVisibleChange(to: false)
+                            })
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: workItem)
+                    }
+                    guard outlet == self?.outletCollection.last else { return }
+                    self?.delegate?.isVisibleChange(to: true)
+                    
+                    // isLocked일 경우 lockButton도 사라져야 함.
+                    let workItem = DispatchWorkItem {
+                        self?.delegate?.checkLockButton()
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: workItem)
+                })
+        }
+    }
+    
+    func fadeOutUI(isLocked: Bool = false) {
+        outletCollection.filter { isLocked ? $0 != lockButton : true }
+            .forEach { outlet in
+                UIView.animate(withDuration: 1.0,
+                               delay: 0.0,
+                               options: [.curveEaseInOut, .allowUserInteraction, .beginFromCurrentState],
+                               animations: {
+                                outlet.alpha = 0.0
+                }, completion: { [weak self] _ in
+                    if isLocked {
+                        let workItem = DispatchWorkItem {
+                            UIView.animate(withDuration: 1.0,
+                                           animations: { [weak self] in
+                                            self?.lockButton.alpha = 0.0
+                                }, completion: { [weak self] _ in
+                                    self?.delegate?.isVisibleChange(to: false)
+                            })
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: workItem)
+                    }
+                    guard outlet == self?.outletCollection.last else { return }
+                    
+                    self?.delegate?.isVisibleChange(to: false)
+                })
+        }
+    }
+    
+    func setLockUI(isLocked: Bool) {
+        delegate?.setLock(isLocked: isLocked)
+        
+        switch isLocked {
+        case true:
+            fadeOutUI(isLocked: true)
+        case false:
+            fadeInUI(isLocked: false)
+        }
+    }
 }
