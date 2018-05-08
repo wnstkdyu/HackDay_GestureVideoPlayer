bcd978e57c3cb48c444f87a21344e8f4f9d2ff5f
alpaca <wnstkdyu@gmail.com>
2018-05-08 21:26:36 +0900

    SwiftLint 설치

---
+++ Pods/Target Support Files/Pods-HackDay_GestureVideoPlayer/Pods-HackDay_GestureVideoPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackDay_GestureVideoPlayer : NSObject
+@end
+@implementation PodsDummy_Pods_HackDay_GestureVideoPlayer
+@end
---
+++ Pods/Target Support Files/Pods-HackDay_GestureVideoPlayer/Pods-HackDay_GestureVideoPlayer-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_HackDay_GestureVideoPlayerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HackDay_GestureVideoPlayerVersionString[];
+
